18e8f3c388fd6f8235e769ffffd448fd48d68d9d
TeaCoder52 <vnechaev888@gmail.com>
2025-12-03 14:34:45 +0300

    chore: update version

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-export * from "./presets/base.js";
-export * from "./presets/typescript.js";
-export * from "./presets/react.js";
-export * from "./presets/nest.js";
-export * from "./presets/markdown.js";
+export * from "./presets/base";
+export * from "./presets/typescript";
+export * from "./presets/react";
+export * from "./presets/nest";
+export * from "./presets/markdown";
 
-export * from "./overrides/json.override.js";
-export * from "./overrides/yaml.override.js";
-export * from "./overrides/graphql.override.js";
+export * from "./overrides/json.override";
+export * from "./overrides/yaml.override";
+export * from "./overrides/graphql.override";
 
-export * from "./utils/merge-config.js";
-export * from "./utils/validate-config.js";
-export * from "./utils/resolve-paths.js";
+export * from "./utils/merge-config";
+export * from "./utils/validate-config";
+export * from "./utils/resolve-paths";
